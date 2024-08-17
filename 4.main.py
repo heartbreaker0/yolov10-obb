2176dc0b0f176c0bc60215bfead546768d758b2f
Test Flight <test@example.com>
2024-08-17 03:17:24 +0100

    update main

--- 4.main.py
+++ 4.main.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 def main():
     model = YOLO("yolov10n-obb.yaml")
 
-    result = model.train(data="/openbayes/home/DOTA-SHIP.yaml", epochs=1000, imgsz=1024, batch=8, device=0, pretrained=False, plots=True)
+    result = model.train(data="/openbayes/home/yolov10-obb/DOTA-SHIP.yaml", epochs=1000, imgsz=1024, batch=8, device=0, pretrained=False, plots=True)
 
 
 if __name__ == "__main__":
